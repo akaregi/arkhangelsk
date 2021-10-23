9fa6260f48bb00b970f10045022a9fe4bc000952
akaregi <verdigris@azure.icu>
2021-10-23 22:34:17 +0900

    fix: don't work on prod
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/boot/command.ts
+++ src/boot/command.ts
@@ -4,10 +4,12 @@ import { readdir } from 'fs/promises'
 import { Command } from '../commands/Command'
 
 export async function getCommands (): Promise<Collection<string, Command>> {
+  console.log(process.cwd())
+
   const commandFiles = (await readdir(join(__dirname, '../commands')))
-    .filter(file => file.endsWith('.ts'))
+    .filter(file => file.endsWith('.ts') || file.endsWith('.js'))
     .map(file => file.slice(0, -3))
-    .map(file => require(process.cwd() + '/app/commands/' + file))
+    .map(file => require(join(__dirname, '../commands', file)))
 
   // Registers available commands
   const commands = new Collection<string, Command>()
